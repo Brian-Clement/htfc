8b355ada80b1340a2feece31425cba9a74fd3750
Brian Clement <brian@bclement.com>
2017-09-27 09:17:19 -0500

    HTFC-1: VM config via blt vm command.

--- drush/site-aliases/aliases.drushrc.php
+++ drush/site-aliases/aliases.drushrc.php
@@ -359,3 +359,33 @@
 #    'uri' => 'http://example.com',
 #    'root' => $aliases['dev']['root'],
 #  ) + $aliases['server'];
+
+// Local environment.
+$aliases['htfc.local'] = array(
+  'root' => '/var/www/htfc/docroot',
+  'uri' => 'http://local.htfc.com',
+  );
+// Add remote connection options when alias is used outside VM.
+if ('vagrant' != $_SERVER['USER']) {
+  $aliases['htfc.local'] += array(
+    'remote-host' => 'local.htfc.com',
+    'remote-user' => 'vagrant',
+    'ssh-options' => '-o PasswordAuthentication=no -i ' . drush_server_home() . '/.vagrant.d/insecure_private_key'
+  );
+}
+
+
+// Local environment.
+$aliases['htfc.local'] = array(
+  'root' => '/var/www/htfc/docroot',
+  'uri' => 'http://local.htfc.com',
+  );
+// Add remote connection options when alias is used outside VM.
+if ('vagrant' != $_SERVER['USER']) {
+  $aliases['htfc.local'] += array(
+    'remote-host' => 'local.htfc.com',
+    'remote-user' => 'vagrant',
+    'ssh-options' => '-o PasswordAuthentication=no -i ' . drush_server_home() . '/.vagrant.d/insecure_private_key'
+  );
+}
+
